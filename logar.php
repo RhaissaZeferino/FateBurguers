16419719e45cc075f82c337478f387393ac12f8e
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-04 21:22:08 -0300

    Add files via upload

---
+++ logar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fateburguers</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Efetue login</h2>
+        <form id="form_login" action="login.php" method="POST">
+            <?php if (isset($resultado) && $resultado["cod"] == 0) : ?>
+                <div class="alert alert-danger">
+                    <?php echo $resultado["msg"]; ?>
+                </div>
+            <?php endif; ?>
+            <input type="email" id="email" name="email" placeholder="Digite seu email" />
+            <br /><br />
+            <input type="password" id="senha" name="senha" placeholder="Digite sua senha" />
+            <br /><br />
+            <input type="submit" id="submeter" value="Entrar" class="btn btn-primary" />
+        </form>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</html>
