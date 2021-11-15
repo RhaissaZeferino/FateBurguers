893a3a5bf1268fb1b01b823bcddb84556ccba8db
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-15 19:07:05 -0300

    Add files via upload

---
+++ cadastrar_adicionais.php
@@ -0,0 +1,34 @@
+<?php
+    if(count($_POST) > 0) {
+        // TELA DE CADASTRO DE PRODUTOS
+        $codigo = $_POST['codigo'];
+        $descricao  = $_POST["descricao"];
+        $preco = $_POST["preco"];
+
+        try {
+            require("conexao.php");
+
+            
+            $sql = "INSERT INTO adicionais 
+                    (codigo, descricao, preco) 
+                    VALUES 
+                    (?,?,?)";
+            $stmt= $conn->prepare($sql);
+            
+            $stmt->execute([$codigo, $descricao, $preco]);
+        
+            $resultado["msg"] = "Item inserido com sucesso!";
+            $resultado["cod"] = 1;
+            $resultado["style"] = "alert-success";
+        }
+        catch(PDOException $e){
+            $resultado["msg"] = "Inserção no banco de dados falhou: " . $e->getMessage();
+            $resultado["cod"] = 0;
+            $resultado["style"] = "alert-danger";
+        }
+        $conn = null;
+    }
+
+    include("index.php");
+
+?>
