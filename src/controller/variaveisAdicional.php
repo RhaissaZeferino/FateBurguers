24ce630e16066c2d5e85c54bd0412050468a24a0
Rhaíssa Zeferino <66739812+RhaissaZeferino@users.noreply.github.com>
2021-12-15 15:48:48 -0300

    Delete src directory

--- src/controller/variaveisAdicional.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$codigo = isset($_POST['codigo'])?$_POST['codigo']:"";
-$descricao = isset($_POST['descricao'])?$_POST['descricao']:"";
-$preco = isset($_POST['preco'])?$_POST['preco']:"";
--- src/controller/variaveisCliente.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$codigo = isset($_POST['codigo'])?$_POST['codigo']:"";
-$nome = isset($_POST['nome'])?$_POST['nome']:"";
-$endereco = isset($_POST['endereco'])?$_POST['endereco']:"";
-$telefone = isset($_POST['telefone'])?$_POST['telefone']:"";
